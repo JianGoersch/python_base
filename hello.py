4b62181b2797f6b876595b3a5362355dc7d60f37
willrosa93 <w_sdr@outlook.com>
2023-08-14 14:54:28 -0300

    alterações

--- hello.py
+++ hello.py
@@ -1,1 +1,40 @@
-print("Hello world!!")
+#!/usr/bin/env python3
+
+"""
+Hello world multilinguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a varialvel LANG devidamente configurada:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__="0.0.1"
+__author__="Jian Goersch"
+__license__="unlicense"
+
+# Dunder == __ 2 undelines
+#snake_case
+#PascalCase
+
+import os
+
+current_language = os.getenv("LANG", "pt_BR")[:5] #Fatia valor da variavel do 0 À 5
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!" 
+elif current_language == "es_SP":
+    msg = "Hola, Mondo!"       
+print(msg)
